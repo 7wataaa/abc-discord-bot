b255455221cdf8a740496b9078b489d5d11e43ba
nashi <nn.contact.n@gmail.com>
2021-08-03 02:04:08 +0900

    add migration.sqlを追加

---
+++ prisma/migrations/20210801185235_/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+  - The primary key for the `send_users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+*/
+-- AlterTable
+ALTER TABLE "send_channel" ALTER COLUMN "server_id" SET DATA TYPE TEXT,
+ALTER COLUMN "channel_id" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "send_users" DROP CONSTRAINT "send_users_pkey",
+ALTER COLUMN "server_id" SET DATA TYPE TEXT,
+ALTER COLUMN "user_id" SET DATA TYPE TEXT,
+ADD PRIMARY KEY ("server_id", "user_id");
