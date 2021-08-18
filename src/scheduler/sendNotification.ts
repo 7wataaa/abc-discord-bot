5778b7afd37ec7116920145d98ff3fc768fec7c0
nashi <nn.contact.n@gmail.com>
2021-08-18 19:33:33 +0900

    add: sendNotificationの変更
    ログを出すようにしたのとデバッグ用の時間の変更

--- src/scheduler/sendNotification.ts
+++ src/scheduler/sendNotification.ts
@@ -5,18 +5,24 @@ import { prisma } from '../prismaClient';
 // TODO Heroku schedulerで10分間隔で起動させる、もし予定された時刻が10分以内だったらその時刻まで待って通知を送信する
 async function sendNotification() {
   // TODO モックからDBにある日付に変更する
-  const target = new Date(2021, 8, 18, 16, 50);
+  const target = new Date(2021, 8, 18, 19 - 9, 36);
 
   // TODO 一応日付またぐ場合の処理しないといけない
   const timeDifference = target.valueOf() - new Date().valueOf();
 
   // 過ぎてしまった時間と、10分以上の差がある場合何もしない
   if (timeDifference < 0 || 600000 <= timeDifference) {
+    console.info('予定が10分未満ではないので何もしない');
     return;
   }
+  console.info(
+    `予定が10分未満なので${timeDifference}ミリ秒待って通知を送信する`
+  );
 
   // 時刻まで待つ
   await sleep(timeDifference);
+
+  console.info(`${timeDifference}ミリ秒待ったので通知を送信`);
 
   // DBに登録してるサーバーのチャンネルに送信してく
   const sendChannels = await prisma.send_channel.findMany();
@@ -24,6 +30,7 @@ async function sendNotification() {
   for (const e of sendChannels) {
     const channel = client.channels.cache!.get(e.channel_id) as TextChannel;
 
+    console.info(channel.name);
     channel.send(`DEBUG用: 時刻は${target}です。`);
   }
 }
