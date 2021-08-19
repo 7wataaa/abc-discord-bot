5ea2505ac02b2ca3084d6f6e92092bc427d67829
nashi <nn.contact.n@gmail.com>
2021-08-19 21:06:52 +0900

    fix: sendNotificationの時間指定を変更

--- src/scheduler/sendNotification.ts
+++ src/scheduler/sendNotification.ts
@@ -10,22 +10,22 @@ import { prisma } from '../prismaClient';
 
 //=================================
 
+const oneDateAsMillisecond = 86400 * 1000;
+
 // TODO Heroku schedulerで10分間隔で起動させる、もし予定された時刻が10分以内だったらその時刻まで待って通知を送信する
 async function sendNotification() {
   // TODO モックからDBにある日付に変更する
-  const target = new Date(2021, 8, 18, 20 - 9, 14);
+  const target = new Date(2021, 8, 19, 21 - 9, 10);
 
   // TODO 一応日付またぐ場合の処理しないといけない
   const timeDifference = target.valueOf() - new Date().valueOf();
 
-  // 過ぎてしまった時間と、10分以上の差がある場合何もしない
-  if (timeDifference < 0 || 600000 <= timeDifference) {
-    console.info('予定が10分未満ではないので何もしない');
+  // 過ぎてしまった時間と、24時間以上の差がある場合何もしない
+  if (!(0 < timeDifference && timeDifference < oneDateAsMillisecond)) {
+    console.info('予定が24時間未満ではないので何もしない');
     return;
   }
-  console.info(
-    `予定が10分未満なので${timeDifference}ミリ秒待って通知を送信する`
-  );
+  console.info(`予定が24時間なので${timeDifference}ミリ秒待って通知を送信する`);
 
   // 時刻まで待つ
   await sleep(timeDifference);
